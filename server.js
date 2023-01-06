007a04e42668215de58f4d581e39d1c9cddb0e72
shubhamparab-uvxcel <shubhamp@uvxcel.com>
2023-01-06 16:12:33 +0530

    Added .env

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-let AZURE_URI = `mongodb://alms-mongodb:LLXgzHFTLDumql8vQgUzBFm6RhRGL4q7NCnTNdcxOAzhxNHxVqwlMA7H9aoWsHPAiErjt0vSFAgL9gqTHi4nDg==@alms-mongodb.mongo.cosmos.azure.com:10255/uvXcelOriginal?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@alms-mongodb@`;
-
 mongoose
-  .connect(AZURE_URI, {
+  .connect(process.env.AZURE_URI, {
     useNewUrlParser: true,
   })
   .then(() => console.log("Connected to Azure cosmos db"))
@@ -26,7 +24,7 @@ app.use("/", require("./routes/jobRoutes"));
 app.use("/", require("./routes/userRoutes"));
 // app.use("/", require("./routes/resourceRoutes"));
 
-const PORT = 10255;
+const PORT = process.env.PORT || 10255;
 
 app.listen(PORT, function () {
   console.log(`server running on port ${PORT}`);
