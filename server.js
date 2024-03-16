39e3cc59f60715724a79663dfafd68768623e65e
Shubham Parab <shubham@smartrr.com>
2024-03-16 11:58:22 +0530

    Removed dependencies

--- server.js
+++ server.js
@@ -3,15 +3,8 @@ require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 app.use(cors());
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 mongoose
   .connect(process.env.AZURE_URI, {
@@ -20,9 +13,7 @@ mongoose
   .then(() => console.log("Connected to Azure cosmos db"))
   .catch(console.error);
 
-// app.use("/", require("./routes/jobRoutes"));
 app.use("/", require("./routes/userRoutes"));
-// app.use("/", require("./routes/resourceRoutes"));
 
 const PORT = process.env.PORT || 10255;
 
